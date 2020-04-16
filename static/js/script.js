05b7fe8f49689b7c874e34acdb789e746159c2c6
Joost Faber <j.faber@hva.nl>
2020-04-16 14:59:31 +0200

    refactored code

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,14 @@
 const timeline = new TimelineMax();
 
-timeline.from(
-  'main',
-  0.5,
-  { scale: 0, opacity: 0.5, ease: 'back.out(1.7)' },
-  '=+.25'
-);
-timeline.from('main > h1', 0.5, { y: -200, opacity: 0 }, '=+.5');
-timeline.from('p', 1, { x: -200, opacity: 0 }, '=-.5');
-timeline.from('section', 0.5, { x: 500, opacity: 0 }, '=-1');
-timeline.staggerFrom('code', 1, { x: '+=150', opacity: 0 }, 0.2, '=-.75');
-timeline.from(
-  'img',
-  2,
-  { x: -200, opacity: 0, rotation: 360, ease: 'elastic.out(1, .3)' },
-  '=+.25'
-);
+timeline
+  .from('main', 0.5, { scale: 0, opacity: 0.5 }, '=+.25')
+  .from('main > h1', 0.5, { y: -200, opacity: 0 }, '=+.5')
+  .from('p', 1, { x: -200, opacity: 0 }, '=-.5')
+  .from('section', 0.5, { x: 500, opacity: 0 }, '=-1')
+  .staggerFrom('code', 1, { x: '+=150', opacity: 0 }, 0.2, '=-.75')
+  .from(
+    'img',
+    2,
+    { x: -200, opacity: 0, rotation: 360, ease: 'elastic.out(1, .3)' },
+    '=+.25'
+  );
