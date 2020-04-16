043fdb7b48be5f959bb4754e0a3e101cb56f1933
Joost Faber <j.faber@hva.nl>
2020-04-16 09:44:05 +0200

    tweaked animations

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,16 @@
-const tl = new TimelineMax();
+const timeline = new TimelineMax();
 
-tl.from('main', 0.5, { scale: 0, opacity: 0.5 }, '=+.25');
-tl.from('h1', 0.5, { y: -200, opacity: 0 }, '=+.5');
-tl.from('p', 1, { x: 200, opacity: 0 }, '=-.5');
-tl.from(
+timeline.from(
+  'main',
+  0.5,
+  { scale: 0, opacity: 0.5, ease: 'back.out(1.7)' },
+  '=+.25'
+);
+timeline.from('main > h1', 0.5, { y: -200, opacity: 0 }, '=+.5');
+timeline.from('p', 1, { x: -200, opacity: 0 }, '=-.5');
+timeline.from('section', 0.5, { x: 500, opacity: 0 }, '=-1');
+timeline.staggerFrom('code', 1, { x: '+=150', opacity: 0 }, 0.2, '=-.75');
+timeline.from(
   'img',
   2,
   { x: -200, opacity: 0, rotation: 360, ease: 'elastic.out(1, .3)' },
