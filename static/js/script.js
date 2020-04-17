b41b346d2b19d45ac5fb638d3c09009a68fb3a1a
Joost Faber <j.faber@hva.nl>
2020-04-17 08:59:06 +0200

    removed yoyo from timeline

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,17 @@ timeline
   .from('main', 0.5, { scale: 0, opacity: 0.5 }, '=+.25')
   .from('main > h1', 0.5, { y: -200, opacity: 0 }, '=+.5')
   .from('p', 1, { x: -200, opacity: 0 }, '=-.5')
-  .from('section', 0.5, { x: 500, opacity: 0 }, '=-1')
+  .from('section', 0.5, { y: 500, opacity: 0 }, '=-1')
   .staggerFrom('code', 1, { x: '+=150', opacity: 0 }, 0.2, '=-.75')
   .from(
     'img',
     2,
-    { x: -200, opacity: 0, rotation: 360, ease: 'elastic.out(1, .3)' },
+    {
+      opacity: 0,
+      rotation: 360,
+      scale: 0,
+      transformOrigin: '50% 50%',
+      ease: 'elastic.out(1, .3)'
+    },
     '=+.25'
   );
